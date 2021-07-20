67c4a0d5e31f1d706628d0ce2d9ae2226508c0bd
panqinzhang <panqin.zhang@thoughtworks.com>
2021-07-20 21:56:22 +0800

    build(N/A): 初始化项目

---
+++ src/main/java/com/fly/spring/contract/study/FlySpringContractApplication.java
@@ -0,0 +1,13 @@
+package com.fly.spring.contract.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlySpringContractApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlySpringContractApplication.class, args);
+    }
+
+}
